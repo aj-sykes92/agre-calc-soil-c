bafcce787d597085645dcdcab3173d11916d3428
aj-sykes-92 <alasdair.sykes@gmail.com>
2019-11-20 16:06:31 +0000

    Overwrote/adapted template code. Wrote script to read in spatial data showing soil type distribution, and UK postcode shapefiles. Processed data to output .csv with 250m grid cells linked to soil type and postcode attributes.

--- AC-soil-C-preprocessing.R
+++ AC-soil-C-preprocessing.R
@@ -2,59 +2,68 @@
 library(raster)
 library(tidyverse)
 library(sp)
+library(rgdal)
 
-data_repo <- "AgRE Calc PLC/"
+data_repo <- "GIS data repository"
 
-UK <- find_onedrive(dir = data_repo, path = "GIS data/DA shapefile/GBR_adm_shp/GBR_adm1.shp") %>% shapefile()
+UK <- find_onedrive(dir = data_repo, path = "DA shapefile/GBR_adm_shp/GBR_adm1.shp") %>% shapefile()
 
-# read in soil raster data and stack
-Soil_stack <- stack(find_onedrive(dir = data_repo, path = "GIS data/SoilGrids 5km/Soil pH/Fixed/PHIHOX_M_sl4_5km_ll.tif") %>% raster(), # pH
-                    find_onedrive(dir = data_repo, path = "GIS data/SoilGrids 5km/Sand content/Fixed/SNDPPT_M_sl4_5km_ll.tif"), # sand %
-                    find_onedrive(dir = data_repo, path = "GIS data/SoilGrids 5km/Silt content/Fixed/SLTPPT_M_sl4_5km_ll.tif"), # silt %
-                    find_onedrive(dir = data_repo, path = "GIS data/SoilGrids 5km/Clay content/Fixed/CLYPPT_M_sl4_5km_ll.tif"), # clay %
-                    find_onedrive(dir = data_repo, path = "GIS data/SoilGrids 5km/OC tonnes per ha/Fixed/OCSTHA_M_sd4_5km_ll.tif")) # OC tonnes per ha
+# below code is commented out — not req'd since masking to UK only need happen once
+soil_wrb <- find_onedrive(dir = data_repo, path = "SoilGrids250/TAXNWRB_250m_ll_UKonly.tif") %>% raster()
 
-# read in crop area raster data and stack
-readdir <- find_onedrive(dir = data_repo, path = "GIS data/MapSPAM data/Physical area")
-file.names <- dir(readdir, pattern =".tif")
+# read in soil raster data
+#soil_wrb <- find_onedrive(dir = data_repo, path = "SoilGrids250/TAXNWRB_250m_ll.tif") %>% raster()
 
-Crop_area_stack <- raster::stack()
-for(i in 1:length(file.names)){
-  readpath <- paste(readdir, file.names[i], sep="/") # aggregate strings to create filepath
-  x <- raster(readpath) # read raster
-  Crop_area_stack <- addLayer(Crop_area_stack, x)
-  rm(x)
-  print(file.names[i])
-}
+# check projections
+#soil_wrb@crs@projargs
+#UK@proj4string@projargs
 
-# read in crop yield raster data and stack
-readdir <- find_onedrive(dir = data_repo, path = "GIS data/MapSPAM data/Yield")
-file.names <- dir(readdir, pattern =".tif")
+# mask to UK only
+#soil_wrb <- soil_wrb %>% crop(UK) %>% mask(UK)
+#writeRaster(soil_wrb, find_onedrive(dir = data_repo, path = "SoilGrids250/TAXNWRB_250m_ll_UKonly.tif"))
 
-Crop_yield_stack <- raster::stack()
-for(i in 1:length(file.names)){
-  readpath <- paste(readdir, file.names[i], sep="/") # aggregate strings to create filepath
-  x <- raster(readpath) # read raster
-  Crop_yield_stack <- addLayer(Crop_yield_stack, x)
-  rm(x)
-  print(file.names[i])
-}
-rm(readdir, file.names, readpath, i)
+# create dataframe from raster
+Dat_main <- soil_wrb %>%
+  as.data.frame(xy = T) %>%
+  as_tibble() %>%
+  rename(wrb_int = TAXNWRB_250m_ll_UKonly) %>%
+  drop_na(wrb_int)
 
-# add area layer
-Area <- Soil_stack[[1]] %>% area()
-Soil_stack <- addLayer(Soil_stack, Area)
-rm(Area)
+# read in legend for WRB and add shortened soil name to main data
+Dat_leg <- find_onedrive(dir = data_repo, path = "SoilGrids250/TAXNWRB_250m_ll_legend.csv") %>% read_csv()
 
-# aggregate
-Master_stack <- stack(Soil_stack, Crop_area_stack, Crop_yield_stack)
-rm(Soil_stack, Crop_area_stack, Crop_yield_stack)
+Dat_main <- Dat_main %>%
+  left_join(Dat_leg %>% select(wrb_int = Number, wrb_name = Group), by = "wrb_int")
 
-# mask out to UK only
-Master_stack <- Master_stack %>% crop(UK) %>% mask(UK)
-plot(Master_stack[[1]])
-# plot(UK, add=T)
+# read in postcode shapefiles from Mike S
+Postcodes <- find_onedrive(dir = data_repo, path = "UK postcodes/postcode_polygons.gpkg") %>% readOGR(layer = "postcode_district")
+Postcodes_wgs84 <- Postcodes %>% spTransform(CRS("+proj=longlat +datum=WGS84")) # reproject
 
-# convert to dataframe
-Dat_main <- Master_stack %>% as.data.frame(xy = T) %>% drop_na(PHIHOX_M_sl4_5km_ll)
-# we have zeros, >0s and NAs in the area data, NAs and >0s only in the yield data
+# rasterise postcodes and assign to main data (code commented and bridged because this takes f*cking ages)
+#template <- find_onedrive(dir = data_repo, path = "SoilGrids250/SoilGrids250_blank_template_UKonly.tif") %>% raster()
+
+#postcodes_raster <- rasterize(Postcodes_wgs84, template, field = Postcodes@data[,1], fun = "mean", 
+#                              update = TRUE, updateValue = "NA")
+
+#writeRaster(postcodes_raster, find_onedrive(dir = data_repo, path = "UK postcodes/UK_postcode_raster_250m.tif"))
+
+# this reads in the results of the commented code above
+postcodes_raster <- find_onedrive(dir = data_repo, path = "UK postcodes/UK_postcode_raster_250m.tif") %>% raster()
+
+UK_postcodes <- tibble(index = Postcodes@data[, 1],
+                       postcode = Postcodes@data[, 2])
+
+# convert postcodes to dataframe and join actual postcodes into data
+temp <- postcodes_raster %>%
+  as.data.frame(xy = T) %>%
+  as_tibble() %>%
+  rename(index = UK_postcode_raster_250m) %>%
+  drop_na(index) %>%
+  left_join(UK_postcodes, by = "index")
+
+# join dataframes together and write out main data as .csv
+Dat_main <- Dat_main %>%
+  left_join(temp, by = c("x", "y"))
+rm(temp)
+
+write_csv(Dat_main, find_onedrive(dir = data_repo, path = "UK postcodes/Soil types by postcode 250m.csv"))
